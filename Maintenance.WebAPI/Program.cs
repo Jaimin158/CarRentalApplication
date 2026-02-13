27010487e3bfc2a1210993ad0484675c5142abf2
Jaimin158 <Jaiminsolanki05@gmail.com>
2026-02-13 00:44:45 -0500

    Maintenance Webapi is added

---
+++ Maintenance.WebAPI/Program.cs
@@ -0,0 +1,24 @@
+using Maintenance.WebAPI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// register fake service
+builder.Services.AddScoped<IRepairHistoryService, FakeRepairHistoryService>();
+
+var app = builder.Build();
+
+// Swagger enabled for Azure too (rubric requirement)
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+
+app.Run();
